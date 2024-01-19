aebc0b5c3d6587186ae5f949bd33493187b82f82
Karan8113 <157044092+Karan8113@users.noreply.github.com>
2024-01-19 18:08:27 +0530

    Delete portfolio website/index.js

--- portfolio website/index.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    
-    const icon = document.getElementById("hamburger-icon");
-    const menu = document.getElementById("menu-links");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
